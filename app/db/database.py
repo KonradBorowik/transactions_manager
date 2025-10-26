12756cb7d649247d77bed4f66525646463d60ac4
KonradBorowik <konrad1borowik@gmail.com>
2025-10-26 22:47:51 +0100

    database operations made into class

--- app/db/database.py
+++ app/db/database.py
@@ -22,69 +22,74 @@ def get_db():
         db.close()
 
 
-def store_in_db(db: Session, tr_model: TransactionModel) -> None:
-    try:
-        db.add(tr_model)
-        db.commit()
-        db.refresh(tr_model)
-    except IntegrityError:
-        db.rollback()
-        print("Row already exists in the database.")
+class DatabaseOperations:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def store(self, tr_model: TransactionModel) -> None:
+        try:
+            self.db.add(tr_model)
+            self.db.commit()
+            self.db.refresh(tr_model)
+        except IntegrityError:
+            self.db.rollback()
+            print("Row already exists in the database.")
+
+    def get_all(
+        self,
+        skip: int,
+        limit: int) -> Union[list[TransactionModel], None]:
+        return self.db.query(TransactionModel).offset(skip).limit(limit).all()
+
+    def get_filtered(
+        self,
+        filters: dict[str, str],
+        skip: int,
+        limit: int) -> list[TransactionModel]:
+        conditions = []
+        for key, value in filters.items():
+            column = getattr(TransactionModel, key)
+            if column:
+                conditions.append(column == value)
+
+        if skip > -1 and limit > -1:
+            query = select(TransactionModel).where(and_(*conditions)).offset(skip).limit(limit)
+        else:
+            query = select(TransactionModel).where(and_(*conditions))
+        
+        return self.db.execute(query).scalars().all()
 
 
-def get_all_from_db(
-    db: Session,
-    skip: int,
-    limit: int) -> Union[list[TransactionModel], None]:
-    return db.query(TransactionModel).offset(skip).limit(limit).all()
+    def get_amount_summary(self, field_name: str, field_id: UUID) -> dict:
+        query = select(
+            TransactionModel.currency,
+            func.sum(TransactionModel.amount).label("total_amount")
+        ).where(
+            getattr(TransactionModel, field_name) == field_id
+        ).group_by(TransactionModel.currency)
+
+        return self.db.execute(query).all()
 
 
-def get_queried_from_db(
-    db: Session,
-    filters: dict[str, str],
-    skip: int,
-    limit: int) -> Union[list[TransactionModel], None]:
-    conditions = []
-    for key, value in filters.items():
-        column = getattr(TransactionModel, key)
-        if column:
-            conditions.append(column == value)
-    if skip > -1 and limit > -1:
-        query = select(TransactionModel).where(and_(*conditions)).offset(skip).limit(limit)
-    else:
-        query = select(TransactionModel).where(and_(*conditions))
-    return db.execute(query).scalars().all()
+    def get_unique_count(self, field_name: str, field_id: UUID, entry_to_count: str) -> int:
+        query = (
+            select(func.count(distinct(getattr(TransactionModel, entry_to_count))))
+            .where(getattr(TransactionModel, field_name) == field_id)
+        )
+
+        return self.db.execute(query).scalar()
 
 
-def get_amount(db: Session, field_name: str, field_id: UUID) -> dict:
-    
-    query = select(
-        TransactionModel.currency,
-        func.sum(TransactionModel.amount).label("total_amount")
-    ).where(getattr(TransactionModel, field_name) == field_id).group_by(TransactionModel.currency)
-
-    return db.execute(query).all()
+    def get_item_count(self, item_id: UUID) -> int:
+        query = select(func.count()).where(TransactionModel.product_id == item_id)
+        
+        return self.db.execute(query).scalar()
 
 
-def get_unique_count(db: Session, field_name: str, field_id: UUID, entry_to_count: str) -> int:
-    query = (
-        select(func.count(distinct(getattr(TransactionModel, entry_to_count))))
-        .where(getattr(TransactionModel, field_name) == field_id)
-    )
+    def get_latest_timestamp(self, field_name: str, filter_id: UUID) -> datetime:
+        query = (
+            select(func.max(TransactionModel.timestamp))
+            .where(getattr(TransactionModel, field_name) == filter_id)
+        )
 
-    return db.execute(query).scalar()
-
-
-def get_item_occurrences(db: Session, item_id: UUID) -> int:
-    query = select(func.count()).where(TransactionModel.product_id == item_id)
-    
-    return db.execute(query).scalar()
-
-
-def get_latest_timestamp(db: Session, field_name: str, filter_id: UUID) -> datetime:
-    query = (
-        select(func.max(TransactionModel.timestamp))
-        .where(getattr(TransactionModel, field_name) == filter_id)
-    )
-
-    return db.execute(query).scalar()
+        return self.db.execute(query).scalar()
--- app/services/db_service.py
+++ app/services/db_service.py
@@ -2,22 +2,14 @@ from sqlalchemy.orm import Session
 from typing import Union
 from uuid import UUID
 
-from app.db.database import (
-    get_all_from_db,
-    get_queried_from_db,
-    store_in_db,
-    get_amount,
-    get_item_occurrences,
-    get_latest_timestamp,
-    get_unique_count
-)
+from app.db.database import DatabaseOperations
 from app.schemas.transaction_schema import Transaction
 from app.models.transaction_model import TransactionModel
 
 
 class DatabaseService:
     def __init__(self, db: Session):
-        self.db = db
+        self.db_ops = DatabaseOperations(db=db)
         self.currency_rates = {
             "EUR": 4.3,
             "USD": 4.0,
@@ -27,8 +19,7 @@ class DatabaseService:
     def store_data(self, data: list[Transaction]) -> None:
         for tr in data:
             tr_model = TransactionModel(**tr.model_dump())
-            store_in_db(db=self.db, tr_model=tr_model)
-                    
+            self.db_ops.store(tr_model=tr_model)
 
     def get_data(
         self,
@@ -36,19 +27,8 @@ class DatabaseService:
         skip: int = -1,
         limit: int = -1) -> list[TransactionModel]:
         if filters:
-            return get_queried_from_db(
-                db=self.db,
-                filters=filters,
-                skip=skip,
-                limit=limit
-            )
-        else:
-            return get_all_from_db(
-                db=self.db,
-                skip=skip,
-                limit=limit
-            )
-
+            return self.db_ops.get_filtered(filters=filters, skip=skip, limit=limit)
+        return self.db_ops.get_all(skip=skip, limit=limit)
 
     def currency_conversion(self, income: dict) -> float:
         total = 0
@@ -57,13 +37,11 @@ class DatabaseService:
             total += amount * rate
         return total
 
-
     def get_product_summary(self, product_id: UUID) -> dict:
-        income_unconverted = get_amount(db=self.db, field_name="product_id", field_id=product_id)
+        income_unconverted = self.db_ops.get_amount_summary(field_name="product_id", field_id=product_id)
         print(income_unconverted)
-        sold_count = get_item_occurrences(db=self.db, item_id=product_id)
-        unique_clients = get_unique_count(
-            db=self.db,
+        sold_count = self.db_ops.get_item_count(item_id=product_id)
+        unique_clients = self.db_ops.get_unique_count(
             field_name="product_id",
             field_id=product_id,
             entry_to_count="customer_id"
@@ -75,16 +53,14 @@ class DatabaseService:
             "unique clients": unique_clients
         }
 
-
     def get_client_summary(self, customer_id: UUID) -> dict:
-        income_unconverted = get_amount(db=self.db, field_name="customer_id", field_id=customer_id)
-        unique_products = get_unique_count(
-            db=self.db,
+        income_unconverted = self.db_ops.get_amount_summary(field_name="customer_id", field_id=customer_id)
+        unique_products = self.db_ops.get_unique_count(
             field_name="customer_id",
             field_id=customer_id,
             entry_to_count="product_id"
         )
-        last_timestamp = get_latest_timestamp(db=self.db, field_name="customer_id", filter_id=customer_id)
+        last_timestamp = self.db_ops.get_latest_timestamp(field_name="customer_id", filter_id=customer_id)
         
         return {
             "total income": self.currency_conversion(income=income_unconverted),
