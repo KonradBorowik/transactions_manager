e78a9dfdd39feed813690a264c702dcb0ab14e1d
KonradBorowik <konrad1borowik@gmail.com>
2025-10-26 19:54:51 +0100

    system accepts only 3 currencies; added example data

--- app/schemas/transaction_schema.py
+++ app/schemas/transaction_schema.py
@@ -1,13 +1,20 @@
+from datetime import datetime
+from enum import Enum
 from pydantic import BaseModel
 from uuid import UUID
-from datetime import datetime
+
+
+class Currency(str, Enum):
+    PLN = "PLN"
+    EUR = "EUR"
+    USD = "USD"
 
 
 class Transaction(BaseModel):
     transaction_id: UUID
     timestamp: datetime
     amount: float
-    currency: str
+    currency: Currency
     customer_id: UUID
     product_id: UUID
     quantity: int
